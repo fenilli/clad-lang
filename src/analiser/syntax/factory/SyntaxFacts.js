124efc16740996d2b27f25a84902cb56c3054bc7
Gustavo Fenilli <gustavofenilli@gmail.com>
2024-01-07 16:28:51 -0300

    feat(parser): :sparkles: adds !, && and || to the expression precedence levels

--- src/analiser/syntax/factory/SyntaxFacts.js
+++ src/analiser/syntax/factory/SyntaxFacts.js
@@ -13,7 +13,8 @@ export class SyntaxFacts {
         switch (kind) {
             case SyntaxKind.PlusToken:
             case SyntaxKind.MinusToken:
-                return 3;
+            case SyntaxKind.BangToken:
+                return 5;
 
             default: return 0;
         };
@@ -28,10 +29,16 @@ export class SyntaxFacts {
         switch (kind) {
             case SyntaxKind.AsteriskToken:
             case SyntaxKind.SlashToken:
-                return 2;
+                return 4;
 
             case SyntaxKind.PlusToken:
             case SyntaxKind.MinusToken:
+                return 3;
+
+            case SyntaxKind.DoubleAmpersandToken:
+                return 2;
+
+            case SyntaxKind.DoublePipeToken:
                 return 1;
 
             default: return 0;
