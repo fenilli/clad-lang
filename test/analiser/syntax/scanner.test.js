283e41d14f207f52a1ba2fcb13ef0305cccaa23c
Gustavo Fenilli <gustavofenilli@gmail.com>
2024-01-09 14:22:52 -0300

    test(scanner): :white_check_mark: makes test for scanner tokens data driven

--- test/analiser/syntax/scanner.test.js
+++ test/analiser/syntax/scanner.test.js
@@ -4,51 +4,90 @@ import assert from 'node:assert';
 import { Scanner } from '../../../src/analiser/syntax/scanner.js';
 import { SyntaxKind } from '../../../src/analiser/syntax/factory/SyntaxKind.js';
 
-const assertTokenHelper = (input = '', ignore = []) => {
-    const scanner = new Scanner(input);
+const tests = [
+    [SyntaxKind.WhitespaceToken, ' '],
+    [SyntaxKind.WhitespaceToken, '  '],
+    [SyntaxKind.WhitespaceToken, '\r'],
+    [SyntaxKind.WhitespaceToken, '\n'],
+    [SyntaxKind.WhitespaceToken, '\r\n'],
 
-    let current = scanner.next();
-    return (expected) => {
-        while (ignore.some((kind) => kind === current.kind)) {
-            current = scanner.next();
-        };
+    [SyntaxKind.NumberToken, '1'],
+    [SyntaxKind.NumberToken, '12'],
 
-        assert.equal(current.kind, expected);
-        current = scanner.next();
-    };
-};
+    [SyntaxKind.IdentifierToken, 'a'],
+    [SyntaxKind.IdentifierToken, 'ab'],
+
+    [SyntaxKind.DoubleAmpersandToken, '&&'],
+    [SyntaxKind.DoublePipeToken, '||'],
+    [SyntaxKind.DoubleEqualToken, '=='],
+    [SyntaxKind.BangEqualToken, '!='],
+    [SyntaxKind.EqualsToken, '='],
+    [SyntaxKind.PlusToken, '+'],
+    [SyntaxKind.MinusToken, '-'],
+    [SyntaxKind.AsteriskToken, '*'],
+    [SyntaxKind.SlashToken, '/'],
+    [SyntaxKind.BangToken, '!'],
+    [SyntaxKind.OpenParenToken, '('],
+    [SyntaxKind.CloseParenToken, ')'],
+
+    [SyntaxKind.TrueKeyword, 'true'],
+    [SyntaxKind.FalseKeyword, 'false'],
+];
+
+/**
+ * @type {[string, string, string[]][]}
+ */
+const specialCases = [
+    [SyntaxKind.WhitespaceToken, SyntaxKind.WhitespaceToken, [SyntaxKind.WhitespaceToken]],
+
+    [SyntaxKind.NumberToken, SyntaxKind.NumberToken, [SyntaxKind.NumberToken]],
+
+    [SyntaxKind.IdentifierToken, SyntaxKind.IdentifierToken, [SyntaxKind.IdentifierToken]],
+    [SyntaxKind.IdentifierToken, SyntaxKind.NumberToken, [SyntaxKind.IdentifierToken]],
+    [SyntaxKind.IdentifierToken, SyntaxKind.TrueKeyword, [SyntaxKind.IdentifierToken]],
+    [SyntaxKind.IdentifierToken, SyntaxKind.FalseKeyword, [SyntaxKind.IdentifierToken]],
+
+    [SyntaxKind.TrueKeyword, SyntaxKind.IdentifierToken, [SyntaxKind.IdentifierToken]],
+    [SyntaxKind.TrueKeyword, SyntaxKind.NumberToken, [SyntaxKind.IdentifierToken]],
+    [SyntaxKind.TrueKeyword, SyntaxKind.TrueKeyword, [SyntaxKind.IdentifierToken]],
+    [SyntaxKind.TrueKeyword, SyntaxKind.FalseKeyword, [SyntaxKind.IdentifierToken]],
+
+    [SyntaxKind.FalseKeyword, SyntaxKind.IdentifierToken, [SyntaxKind.IdentifierToken]],
+    [SyntaxKind.FalseKeyword, SyntaxKind.NumberToken, [SyntaxKind.IdentifierToken]],
+    [SyntaxKind.FalseKeyword, SyntaxKind.FalseKeyword, [SyntaxKind.IdentifierToken]],
+    [SyntaxKind.FalseKeyword, SyntaxKind.TrueKeyword, [SyntaxKind.IdentifierToken]],
+
+    [SyntaxKind.EqualsToken, SyntaxKind.EqualsToken, [SyntaxKind.DoubleEqualToken]],
+    [SyntaxKind.EqualsToken, SyntaxKind.DoubleEqualToken, [SyntaxKind.DoubleEqualToken, SyntaxKind.EqualsToken]],
+
+    [SyntaxKind.BangToken, SyntaxKind.EqualsToken, [SyntaxKind.BangEqualToken]],
+    [SyntaxKind.BangToken, SyntaxKind.DoubleEqualToken, [SyntaxKind.BangEqualToken, SyntaxKind.EqualsToken]],
+];
 
 describe('Scanner', () => {
-    it('scans whitespace token', () => {
-        const assertToken = assertTokenHelper(' ');
-        assertToken(SyntaxKind.WhitespaceToken);
+    it('scans all single tokens', () => {
+        for (const [kind, input] of tests) {
+            const scanner = new Scanner(input);
+            assert.equal(kind, scanner.next().kind);
+        };
     });
 
-    it('scans keywords tokens', () => {
-        const assertToken = assertTokenHelper('true false', [SyntaxKind.WhitespaceToken]);
-        assertToken(SyntaxKind.TrueKeyword);
-        assertToken(SyntaxKind.FalseKeyword);
-    });
+    it('scans all pairs tokens', () => {
+        for (const [kindL, inputL] of tests) {
+            for (const [kindR, inputR] of tests) {
+                const scanner = new Scanner(`${inputL}${inputR}`);
 
-    it('scans symbol tokens', () => {
-        const assertToken = assertTokenHelper('!= == && || = + - * / ! ( )', [SyntaxKind.WhitespaceToken]);
-        assertToken(SyntaxKind.BangEqualToken);
-        assertToken(SyntaxKind.DoubleEqualToken);
-        assertToken(SyntaxKind.DoubleAmpersandToken);
-        assertToken(SyntaxKind.DoublePipeToken);
-        assertToken(SyntaxKind.EqualsToken);
-        assertToken(SyntaxKind.PlusToken);
-        assertToken(SyntaxKind.MinusToken);
-        assertToken(SyntaxKind.AsteriskToken);
-        assertToken(SyntaxKind.SlashToken);
-        assertToken(SyntaxKind.BangToken);
-        assertToken(SyntaxKind.OpenParenToken);
-        assertToken(SyntaxKind.CloseParenToken);
-    });
+                let toTest = [kindL, kindR];
+                for (const [combineL, combineR, kinds] of specialCases) {
+                    if (combineL !== kindL || combineR !== kindR) continue;
 
-    it('scans literals tokens', () => {
-        const assertToken = assertTokenHelper('10 x', [SyntaxKind.WhitespaceToken]);
-        assertToken(SyntaxKind.NumberToken);
-        assertToken(SyntaxKind.IdentifierToken);
+                    toTest = kinds;
+                };
+
+                for (const testKind of toTest) {
+                    assert.equal(testKind, scanner.next().kind);
+                };
+            };
+        };
     });
 });
